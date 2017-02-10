4cd4641cc7cc81d32f192aa64aaeec852dafc9a4
Cris Cecka <criscecka@gmail.com>
2017-02-09 17:27:25 -0800

    NVCC/EDG Workaround...

--- blam/adl/detail/customization_point.h
+++ blam/adl/detail/customization_point.h
@@ -5,6 +5,44 @@
 #include <blam/adl/detail/static_const.h>
 #include <blam/adl/detail/has_adl.h>
 
+#define BLAM_CUSTOMIZATION_POINT(NAME)                                  \
+  namespace detail {                                                    \
+  tag NAME (...);                                                       \
+                                                                        \
+  template <typename R>                                                 \
+  struct NAME##_fn {                                                    \
+    template <typename... T>                                            \
+    static R call(T&&... t) {                                           \
+      return NAME (std::forward<T>(t)...);                              \
+    }                                                                   \
+  };                                                                    \
+                                                                        \
+  template <>                                                           \
+  struct NAME##_fn<tag> {                                               \
+    template <typename... T>                                            \
+    static auto call(T&&... t)                                          \
+        -> decltype(blam::system::generic::NAME(std::forward<T>(t)...)) { \
+      return blam::system::generic::NAME(std::forward<T>(t)...);        \
+    }                                                                   \
+  };                                                                    \
+                                                                        \
+  struct _##NAME {                                                      \
+    template <typename... T>                                            \
+    void operator()(T&&... t) const {                                   \
+      NAME##_fn<decltype( NAME (std::declval<T>()...))>::call(std::forward<T>(t)...); \
+    }                                                                   \
+  };                                                                    \
+  }                                                                     \
+                                                                        \
+  namespace {                                                           \
+  constexpr auto const& NAME = detail::static_const<detail::_##NAME>::value; \
+  }
+
+
+#if 0
+/**
+ * XXX TODO: This version is preferred, but exposes an EDG bug...
+ **/
 #define BLAM_CUSTOMIZATION_POINT(NAME)                                   \
   namespace detail {                                                     \
   tag NAME (...);                                                        \
@@ -28,3 +66,4 @@
   namespace {                                                            \
   constexpr auto const& NAME = detail::static_const<detail::_##NAME>::value; \
   }
+#endif
