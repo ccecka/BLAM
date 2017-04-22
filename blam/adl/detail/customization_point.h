cd3c1bbe38d224db75ed1f6f638a2eeb6e560d6d
Cris Cecka <criscecka@gmail.com>
2017-04-21 17:35:03 -0700

    NVCC customization point fix

--- blam/adl/detail/customization_point.h
+++ blam/adl/detail/customization_point.h
@@ -30,67 +30,34 @@
 #include <utility>  // std::forward
 
 #include <blam/adl/detail/static_const.h>
-#include <blam/adl/detail/has_adl.h>
 
-#define BLAM_CUSTOMIZATION_POINT(NAME)                                  \
-  namespace detail {                                                    \
-  tag NAME (...);                                                       \
-                                                                        \
-  template <typename R>                                                 \
-  struct NAME##_fn {                                                    \
-    template <typename... T>                                            \
-    static R call(T&&... t) {                                           \
-      return NAME (std::forward<T>(t)...);                              \
-    }                                                                   \
-  };                                                                    \
-                                                                        \
-  template <>                                                           \
-  struct NAME##_fn<tag> {                                               \
-    template <typename... T>                                            \
-    static auto call(T&&... t)                                          \
-        -> decltype(blam::system::generic::NAME(std::forward<T>(t)...)) { \
-      return blam::system::generic::NAME(std::forward<T>(t)...);        \
-    }                                                                   \
-  };                                                                    \
-                                                                        \
-  struct _##NAME {                                                      \
-    template <typename... T>                                            \
-    void operator()(T&&... t) const {                                   \
-      NAME##_fn<decltype( NAME (std::declval<T>()...))>::call(std::forward<T>(t)...); \
-    }                                                                   \
-  };                                                                    \
-  }                                                                     \
-                                                                        \
-  namespace {                                                           \
-  constexpr auto const& NAME = detail::static_const<detail::_##NAME>::value; \
+#define BLAM_CUSTOMIZATION_POINT(NAME)                                         \
+  namespace detail {                                                           \
+  namespace generic = blam::system::generic;                                   \
+                                                                               \
+  struct _##NAME {                                                             \
+   private:                                                                    \
+    template <class... T,                                                      \
+              class R = decltype(NAME (std::declval<T>()...))>                 \
+    static constexpr R impl(int, T&&... t) {                                   \
+      return NAME (std::forward<T>(t)...);                                     \
+    }                                                                          \
+                                                                               \
+    template <class... T,                                                      \
+              class R = decltype(generic::NAME (std::declval<T>()...))>        \
+    static constexpr R impl(long, T&&... t) {                                  \
+      return generic::NAME (std::forward<T>(t)...);                            \
+    }                                                                          \
+   public:                                                                     \
+    template <typename... T>                                                   \
+    constexpr auto operator()(T&&... t) const                                  \
+        -> decltype(impl(42, std::forward<T>(t)...)) {                         \
+      return impl(42, std::forward<T>(t)...);                                  \
+    }                                                                          \
+  };                                                                           \
+                                                                               \
+  }                                                                            \
+                                                                               \
+  namespace {                                                                  \
+  constexpr auto const& NAME = detail::static_const<detail::_##NAME>::value;   \
   }
-
-
-#if 0
-/**
- * XXX TODO: This version is preferred, but exposes an EDG bug...
- **/
-#define BLAM_CUSTOMIZATION_POINT(NAME)                                   \
-  namespace detail {                                                     \
-  tag NAME (...);                                                        \
-                                                                         \
-  struct _##NAME {                                                       \
-    template <class... T>                                                \
-    constexpr auto operator()(T&&... t) const                            \
-        -> has_an_adl<decltype( NAME (std::forward<T>(t)...))> {         \
-      return NAME (std::forward<T>(t)...);                               \
-    }                                                                    \
-                                                                         \
-    template <class... T>                                                \
-    constexpr auto operator()(T&&... t) const                            \
-        -> has_no_adl<decltype( NAME (std::forward<T>(t)...)),           \
-                      decltype(blam::system::generic::NAME (std::forward<T>(t)...))> { \
-      return blam::system::generic::NAME (std::forward<T>(t)...);        \
-    }                                                                    \
-  };                                                                     \
-  }                                                                      \
-                                                                         \
-  namespace {                                                            \
-  constexpr auto const& NAME = detail::static_const<detail::_##NAME>::value; \
-  }
-#endif
